014e38fee60c9f7219fe68aceca440296bf7fcf8
freeefly <freefly3557@gmail.com>
2020-04-27 14:58:01 +0900

    feat : rsa 키 교환방식으로 수정

--- src/main/java/com/security/resourceserver/security/Oauth2ResourceServerConfig.java
+++ src/main/java/com/security/resourceserver/security/Oauth2ResourceServerConfig.java
@@ -1,15 +1,20 @@
 package com.security.resourceserver.security;
 
 
+import io.micrometer.core.instrument.util.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.io.IOException;
 
 @Configuration
 @EnableResourceServer
@@ -37,7 +42,14 @@ public class Oauth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey(signKey);
+        Resource resource = new ClassPathResource("publicKey.txt");
+        String publicKey = null;
+        try {
+            publicKey = IOUtils.toString(resource.getInputStream());
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+        converter.setVerifierKey(publicKey);
         return converter;
     }
 }
